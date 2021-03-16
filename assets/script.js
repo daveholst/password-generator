b3d896ef6bf4b74c11ca301a7694a856f7d278ec
Dave Holst <daveholst@gmail.com>
2021-03-16 01:52:31 +0000

    reset file to restart to meet acceptance criteria

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,14 @@
 // Assignment Code
-let generateBtn = document.querySelector("#generate");
-let lengthInput = document.querySelector('#password-length')
-
-// Generate password of desired length
-function generatePassword(length) {
-  let passwordLength = length;
-  let characterSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  let specialCharacterSet = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-  let allChar = characterSet + specialCharacterSet;
-  let generated = '';
-
-  for (i = 0; i < passwordLength; i++){
-    generated += allChar.charAt(Math.floor(Math.random() * allChar.length));
-  }
-  return generated;
-}
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword(lengthInput.value);
-  let passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// Add event listener to range length change that updates
-lengthInput.addEventListener("change", () => {
-  currentSelectedLength = lengthInput.value
-  generateBtn.innerText = `Generate Password [${currentSelectedLength}]`;
-})
